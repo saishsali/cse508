06864b4b1b41916c40d31f391708e683d3514317
saishsali <salisaish@gmail.com>
2017-11-03 23:52:36 -0400

    Working socket communication

--- hw3/pbproxy.c
+++ hw3/pbproxy.c
@@ -4,6 +4,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <string.h>
+
+struct args {
+    int sock;
+    char *key; // Make constant
+};
+
+typedef struct args args;
 
 // https://stackoverflow.com/questions/14002954/c-programming-how-to-read-the-whole-file-contents-into-a-buffer
 char *read_keyfile(char *keyfile) {
@@ -25,11 +33,17 @@ char *read_keyfile(char *keyfile) {
     return buffer;
 }
 
-void server_side_proxy(int listen_port, struct hostent *destination_host, int destination_port) {
-    int sock_fd;
-    struct sockaddr_in address;
+void server_thread(void *args) {
 
-    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+}
+
+void server_side_proxy(int listen_port, struct hostent *destination_host, int destination_port, char *key) {
+    int sock_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         fprintf(stderr, "Socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -37,7 +51,7 @@ void server_side_proxy(int listen_port, struct hostent *destination_host, int de
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = listen_port;
 
-    if (bind(sock_fd, (struct sockaddr *)&address, sizeof(address))) {
+    if (bind(sock_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         fprintf(stderr, "Binding failed");
         exit(EXIT_FAILURE);
     }
@@ -46,10 +60,49 @@ void server_side_proxy(int listen_port, struct hostent *destination_host, int de
         fprintf(stderr, "Listen failed");
         exit(EXIT_FAILURE);
     }
+
+    pthread_t tid;
+    while (1) {
+        // args *arg = (args *)malloc(sizeof(args));
+        // if ((arg->sock = accept(sock_fd, (struct sockaddr *)&address, sizeof(address))) < 0) {
+        //     fprintf(stderr, "Accept failed");
+        //     free(arg);
+        //     exit(EXIT_FAILURE);
+        // }
+        // arg->key = key;
+        // pthread_create(&tid, 0, server_thread, (void*)arg);
+
+
+        if ((new_socket = accept(sock_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            fprintf(stderr, "Accept failed");
+            // free(arg);
+            exit(EXIT_FAILURE);
+        }
+        read(new_socket , buffer, 1024);
+        printf("%s\n", buffer);
+    }
 }
 
-void client_side_proxy() {
+void client_side_proxy(struct hostent *destination_host, int destination_port) {
+    int sock_fd;
+    struct sockaddr_in address;
+    char *hello = "Hello from client";
 
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        fprintf(stderr, "Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = ((struct in_addr *)(destination_host->h_addr))->s_addr;
+    address.sin_port = destination_port;
+
+    if (connect(sock_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        fprintf(stderr, "Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    send(sock_fd, hello, strlen(hello), 0);
 }
 
 int main(int argc, char *argv[]) {
@@ -94,9 +147,9 @@ int main(int argc, char *argv[]) {
     }
 
     if (server == 1) {
-        server_side_proxy(listen_port, destination_host, destination_port);
+        server_side_proxy(listen_port, destination_host, destination_port, keybuffer);
     } else {
-        client_side_proxy();
+        client_side_proxy(destination_host, destination_port);
     }
 
     return 0;
