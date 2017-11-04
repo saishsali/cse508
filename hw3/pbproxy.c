c13659d9787bd9d6b2942dd5ac45fe069383460e
saishsali <salisaish@gmail.com>
2017-11-04 16:49:41 -0400

    Working ssh connection via proxy

--- hw3/pbproxy.c
+++ hw3/pbproxy.c
@@ -12,8 +12,9 @@
 #include <openssl/rand.h>
 #include <openssl/hmac.h>
 #include <openssl/buffer.h>
+#include <pthread.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 4096
 
 struct ctr_state {
     unsigned char ivec[AES_BLOCK_SIZE];  /* ivec[0..7] is the IV, ivec[8..15] is the big-endian counter */
@@ -21,12 +22,15 @@ struct ctr_state {
     unsigned char ecount[AES_BLOCK_SIZE];
 };
 
-struct args {
-    int sock;
-    char *key; // Make constant
+struct proxy_connection {
+    int sock_fd;
+    const char *key;
+    struct hostent *destination_host;
+    int destination_port;
+    struct sockaddr_in ssh_address;
+    struct sockaddr address;
+    int addr_len;
 };
-
-typedef struct args args;
 
 // https://stackoverflow.com/questions/3141860/aes-ctr-256-encryption-mode-of-operation-on-openssl
 void init_ctr(struct ctr_state *state, const unsigned char iv[]) {
@@ -46,7 +50,7 @@ char *read_keyfile(char *keyfile) {
     FILE *fp = fopen(keyfile, "r");
 
     if (fp == NULL) {
-        fprintf(stderr, "%s: Cannot open file", keyfile);
+        fprintf(stderr, "%s: Cannot open file\n", keyfile);
         return NULL;
     }
 
@@ -60,11 +64,6 @@ char *read_keyfile(char *keyfile) {
 
     return buffer;
 }
-
-void server_thread(void *args) {
-
-}
-
 
 void send_data(int sock_fd, AES_KEY aes_key, char buffer[], int size) {
     struct ctr_state state;
@@ -86,7 +85,7 @@ void send_data(int sock_fd, AES_KEY aes_key, char buffer[], int size) {
     write(sock_fd, encrypted_data, size + 8);
 }
 
-void receive_data(int sock_fd, AES_KEY aes_key, char buffer[], int size) {
+void receive_data(int fd, AES_KEY aes_key, char buffer[], int size) {
     struct ctr_state state;
     unsigned char iv[8];
 
@@ -97,121 +96,172 @@ void receive_data(int sock_fd, AES_KEY aes_key, char buffer[], int size) {
 
     AES_ctr128_encrypt(buffer + 8, decrypted_data, size - 8, &aes_key, state.ivec, state.ecount, &state.num);
 
-    write(STDOUT_FILENO, decrypted_data, size - 8);
+    write(fd, decrypted_data, size - 8);
 }
 
-void server_side_proxy(int listen_port, struct hostent *destination_host, int destination_port, char *key) {
-    int sock_fd, new_socket, n, opt = 1;
+void* server_thread(void *args) {
+    struct proxy_connection *connection = (struct proxy_connection *)args;
     struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    int ssh_sock_fd, n, ssh_conn = 0;
 
-    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        fprintf(stderr, "Socket creation failed");
-        exit(EXIT_FAILURE);
+    printf("Server thread started\n");
+
+    if ((ssh_sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        fprintf(stderr, "SSH socket creation failed\n");
+        pthread_exit(0);
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = listen_port;
+    // address.sin_family = AF_INET;
+    // address.sin_addr.s_addr = ((struct in_addr *)(connection->destination_host->h_addr))->s_addr;
+    // address.sin_port = htons(connection->destination_port);
+    // printf("Destination port: %d\n", connection->destination_port);
 
-    if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        // fprintf(stderr, "Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (bind(sock_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        fprintf(stderr, "Binding failed");
-        exit(EXIT_FAILURE);
+    if (connect(ssh_sock_fd, (struct sockaddr *)&connection->ssh_address, sizeof(connection->ssh_address)) < 0) {
+        fprintf(stderr, "SSH connection failed\n");
+        pthread_exit(0);
     }
 
-    if (listen(sock_fd, 3) < 0) {
-        fprintf(stderr, "Listen failed");
-        exit(EXIT_FAILURE);
-    }
+    int flags = fcntl(connection->sock_fd, F_GETFL, 0);
+    fcntl(connection->sock_fd, F_SETFL, flags | O_NONBLOCK);
 
-    // pthread_t tid;
-
-
-    // fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
-    // fcntl(sock_fd, F_SETFL, O_NONBLOCK);
+    flags = fcntl(ssh_sock_fd, F_GETFL, 0);
+    fcntl(ssh_sock_fd, F_SETFL, flags | O_NONBLOCK);
 
     char buffer[BUFFER_SIZE];
     AES_KEY aes_key;
 
-    if (AES_set_encrypt_key(key, 128, &aes_key) < 0) {
-        fprintf(stderr, "Error in setting encryption key");
-        exit(EXIT_FAILURE);
-    }
-//
-    if ((new_socket = accept(sock_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        fprintf(stderr, "Accept failed");
-        // free(arg);
-        exit(EXIT_FAILURE);
+    if (AES_set_encrypt_key(connection->key, 128, &aes_key) < 0) {
+        fprintf(stderr, "Error in setting encryption key\n");
+        pthread_exit(0);
     }
 
-    // fcntl(new_socket, F_SETFL, O_NONBLOCK);
     while (1) {
-        while ((n = read(new_socket, buffer, BUFFER_SIZE)) > 0) {
-            receive_data(sock_fd, aes_key, buffer, n);
+        while ((n = read(connection->sock_fd, buffer, BUFFER_SIZE)) > 0) {
+            receive_data(ssh_sock_fd, aes_key, buffer, n);
+            // write(ssh_sock_fd, buffer, n);
+            if (n < BUFFER_SIZE)
+                break;
         }
-        // args *arg = (args *)malloc(sizeof(args));
-        // if ((arg->sock = accept(sock_fd, (struct sockaddr *)&address, sizeof(address))) < 0) {
-        //     fprintf(stderr, "Accept failed");
-        //     free(arg);
-        //     exit(EXIT_FAILURE);
-        // }
-        // arg->key = key;
-        // pthread_create(&tid, 0, server_thread, (void*)arg);
 
+        while ((n = read(ssh_sock_fd, buffer, BUFFER_SIZE)) >= 0) {
+            if (n > 0) {
+                send_data(connection->sock_fd, aes_key, buffer, n);
+                // write(connection->sock_fd, buffer, n);
+            }
 
+            if (ssh_conn = 0 && n == 0)
+                ssh_conn = 1;
 
-        // read(new_socket , buffer, 1024);
-        // printf("%s\n", buffer);
+            if (n < BUFFER_SIZE)
+                break;
+        }
+
+        if (ssh_conn == 1)
+            break;
+    }
+}
+
+void server_side_proxy(int listen_port, struct hostent *destination_host, int destination_port, char *key) {
+    int sock_fd, new_socket, n, opt = 1;
+    struct sockaddr_in address, ssh_address;
+    int addr_len = sizeof(address);
+
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        fprintf(stderr, "Socket creation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(listen_port);
+
+    ssh_address.sin_family = AF_INET;
+    ssh_address.sin_addr.s_addr = ((struct in_addr *)(destination_host->h_addr))->s_addr;
+    ssh_address.sin_port = htons(destination_port);
+
+    if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        fprintf(stderr, "Reuse address/port failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(sock_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        fprintf(stderr, "Binding failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(sock_fd, 10) < 0) {
+        fprintf(stderr, "Listen failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct proxy_connection *connection;
+    pthread_t tid;
+
+    while (1) {
+        connection = (struct proxy_connection *)malloc(sizeof(struct proxy_connection));
+        if ((connection->sock_fd = accept(sock_fd, &connection->address, &connection->addr_len)) > 0) {
+        // if ((connection->sock_fd = accept(sock_fd, (struct sockaddr *)&address, (socklen_t *)&addr_len)) > 0) {
+            // connection->destination_host = destination_host;
+            // connection->destination_port = destination_port;
+            connection->ssh_address = ssh_address;
+            connection->key = key;
+            pthread_create(&tid, 0, server_thread, (void *)connection);
+            pthread_detach(tid);
+        } else {
+            free(connection);
+        }
     }
 }
 
 void client_side_proxy(struct hostent *destination_host, int destination_port, char *key) {
     int sock_fd, n;
     struct sockaddr_in address;
-    char *hello = "Hello from client";
 
     if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        fprintf(stderr, "Socket creation failed");
+        fprintf(stderr, "Socket creation failed\n");
         exit(EXIT_FAILURE);
     }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = ((struct in_addr *)(destination_host->h_addr))->s_addr;
-    address.sin_port = destination_port;
+    address.sin_port = htons(destination_port);
 
     if (connect(sock_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        fprintf(stderr, "Connection failed");
+        fprintf(stderr, "Connection failed\n");
         exit(EXIT_FAILURE);
     }
 
-    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
-    // fcntl(sock_fd, F_SETFL, O_NONBLOCK);
+    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+
+    flags = fcntl(sock_fd, F_GETFL, 0);
+    fcntl(sock_fd, F_SETFL, flags | O_NONBLOCK);
 
     char buffer[BUFFER_SIZE];
     AES_KEY aes_key;
 
     if (AES_set_encrypt_key(key, 128, &aes_key) < 0) {
-        fprintf(stderr, "Error in setting encryption key");
+        fprintf(stderr, "Error in setting encryption key\n");
         exit(EXIT_FAILURE);
     }
 
     while (1) {
         while ((n = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
             send_data(sock_fd, aes_key, buffer, n);
+            // write(sock_fd, buffer, n);
+            if (n < BUFFER_SIZE)
+                break;
         }
 
-        // while ((n = read(sock_fd, buffer, BUFFER_SIZE)) > 0) {
-        //     receive_data(sock_fd, aes_key, buffer, BUFFER_SIZE);
-        // }
+        while ((n = read(sock_fd, buffer, BUFFER_SIZE)) > 0) {
+            receive_data(STDOUT_FILENO, aes_key, buffer, n);
+            // write(STDOUT_FILENO, buffer, n);
+            if (n < BUFFER_SIZE)
+                break;
+        }
     }
 }
 
 int main(int argc, char *argv[]) {
-    int option, server = 0, listen_port, destination_port;
+    int option, server_mode = 0, listen_port, destination_port;
     char *keyfile = NULL;
     char *keybuffer;
     struct hostent *destination_host;
@@ -219,7 +269,7 @@ int main(int argc, char *argv[]) {
     while ((option = getopt(argc, argv, "l:k:")) != -1) {
         switch (option) {
             case 'l':
-                server = 1;
+                server_mode = 1;
                 listen_port = atoi(optarg);
                 break;
             case 'k':
@@ -232,26 +282,26 @@ int main(int argc, char *argv[]) {
 
     if (optind == argc - 2) {
         if ((destination_host = gethostbyname(argv[optind])) == 0) {
-            fprintf(stderr, "%s: Invalid destination specified", argv[0]);
+            fprintf(stderr, "%s: Invalid destination specified\n", argv[0]);
             exit(EXIT_FAILURE);
         }
         destination_port = atoi(argv[optind + 1]);
     } else {
-        fprintf(stderr, "%s: Invalid destination and port", argv[0]);
+        fprintf(stderr, "%s: Invalid destination and port\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     if (keyfile == NULL) {
-        fprintf(stderr, "%s: Please specify keyfile", argv[0]);
+        fprintf(stderr, "%s: Please specify keyfile\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     if ((keybuffer = read_keyfile(keyfile)) == NULL) {
-        fprintf(stderr, "%s: Read from key file failed", argv[0]);
+        fprintf(stderr, "%s: Read from key file failed\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    if (server == 1) {
+    if (server_mode == 1) {
         server_side_proxy(listen_port, destination_host, destination_port, keybuffer);
     } else {
         client_side_proxy(destination_host, destination_port, keybuffer);
